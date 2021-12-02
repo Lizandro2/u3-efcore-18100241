c6ca64ccedf21864a22e1d809701f0fc8a8b24e1
cotyg <cotyg@DESKTOP-UCJ5KPC>
2021-12-02 08:48:55 -0600

    Add project files.

---
+++ u3-efcore-18100241/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace u3_efcore_18100241.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
